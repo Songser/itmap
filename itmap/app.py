02e0f64541b775060916a894fd8a6086a8f738d0
zhangrj <zhangrj@ethicall.cn>
2018-04-17 22:56:21 +0800

    fix for docker

--- itmap/app.py
+++ itmap/app.py
@@ -1,9 +1,11 @@
 # coding=utf-8
 
+import sys
+print(sys.path)
+import click
 from flask import Flask
-from ext import mail, redis, login_manager
-import config as _config
-
+from itmap.ext import db, mail, redis, login_manager
+import itmap.config as _config
 
 
 def create_app():
@@ -12,17 +14,28 @@ def create_app():
 
     ####
 
+    db.init_app(app)
     mail.init_app(app)
     redis.init_app(app)
     login_manager.init_app(app)
 
     return app
 
+
 app = create_app()
-db = SQLAlchemy(app)
+
+
+@app.cli.command()
+def initdb():
+    click.echo('Init the db')
+
+
+@app.cli.command()
+def sys_path():
+    import sys
+    click.echo('{}'.format(sys.path))
+
 
 @app.route('/', methods=['GET'])
 def index():
     return '<h1>Hello World</h1>', 200
-#if __name__ == '__main__':
-#    app.run(debug=True)
