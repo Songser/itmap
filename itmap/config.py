3439acb8ce27806bd75f36726341547b84ba121a
zhangrj <zhangrj@ethicall.cn>
2018-04-24 21:35:21 +0800

    add smtp server confi

--- itmap/config.py
+++ itmap/config.py
@@ -1,18 +1,29 @@
 # coding=utf-8
 
-# import datetime
+import datetime
 
 DEBUG = False
 
 SECRET_KEY = '...'
-JWT_SECRET_KEY = '...'
 
-# JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(seconds=60 * 60 * 24)
+# -- jwt --
+JWT_SECRET_KEY = '...'
+JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(seconds=60 * 60)
 
 ITMAP_ADMINS = ['rujiazhang@foxmail.com', 'songjiyi2008@163.com']
 ITMAP_ADMIN_PASSWORD = 'Aa@123456?'
 
+# -- postgres --
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@postgres:5432/postgres"
 
+# -- redis --
 REDIS_URL = "redis://@redis:6379/0"
+
+# -- email --
+MAIL_SERVER = 'smtp.qq.com'
+MAIL_USE_SSL = True
+MAIL_PORT = 465
+MAIL_USERNAME = ''
+MAIL_PASSWORD = ''
+MAIL_DEFAULT_SENDER = ('itmap', '')
